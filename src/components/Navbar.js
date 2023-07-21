aff0071577d1d609f86313b7c516f164fe803ab2
Shubham Sonawane <82930194+shubhzhs@users.noreply.github.com>
2023-07-21 17:38:37 +0530

    added navbar, homepage, exercisedetail, footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
+
+const Navbar = () => {
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Navbar;
