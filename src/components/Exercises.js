62f176fa5c2fee70788088ee2aa03e1c018401b6
Shubham Sonawane <82930194+shubhzhs@users.noreply.github.com>
2023-07-29 11:18:40 +0530

    exerciseCard is connected or exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,6 +3,7 @@ import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
@@ -15,6 +16,16 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       >
         Showing Results
       </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
     </Box>
   );
 };
