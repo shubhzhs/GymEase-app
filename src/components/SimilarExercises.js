8176e786928d63a6a79e1a9cb71ff98e58c240d0
Shubham Sonawane <82930194+shubhzhs@users.noreply.github.com>
2023-07-31 20:36:58 +0530

    added the footer

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -6,7 +6,7 @@ import Loader from "./Loader";
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3" mb={5}>
+      <Typography variant="h4" mb={5} ml={4}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction={"row"} sx={{ p: "2", position: "relative" }} mb={5}>
@@ -16,7 +16,7 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
           <Loader />
         )}
       </Stack>
-      <Typography variant="h3" mb={5}>
+      <Typography variant="h4" mb={5} ml={4}>
         Exercises that target the same equipment
       </Typography>
       <Stack direction={"row"} sx={{ p: "2", position: "relative" }}>
